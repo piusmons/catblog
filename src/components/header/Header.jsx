4e1fa19638a3f4ef7b47aa4a25d00f0e0f0e3e44
piusmons <74440275+piusmons@users.noreply.github.com>
2023-04-26 15:55:43 +0800

    change title

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div className="header">
             <div className="headerTitles">
                 <span className="HeaderTitlesm">React & Firebase</span>
-                <span className="HeaderTitlelg">Blog</span>
+                <span className="HeaderTitlelg">Cat News</span>
                 <img className="headerImg" src="https://images.unsplash.com/photo-1542906453-02e875f65428?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"></img>
             </div>
         </div>
